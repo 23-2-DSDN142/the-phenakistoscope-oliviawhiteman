22fa48e59e48704b59596d056ef6194087f32cb1
oliviawhiteman <oliviarosew04@gmail.com>
2023-09-10 23:03:27 +1200

    Trying things out
    start with the swirl with squares template and changed a couple things

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,4 +1,4 @@
-const SLICE_COUNT = 10;
+const SLICE_COUNT = 15;
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
@@ -6,58 +6,35 @@ function setup_pScope(pScope){
   pScope.draw_layer_boundaries(true);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
-  pScope.load_image("heart1" , "png");
-  pScope.load_image("kissy" , "png");
+  pScope.load_image("streak" , "png");
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null,11, 11, 38);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(hearts);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
-
-  var layer2 = new PLayer(kissy);
-  layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
-}
-
-function hearts(x,y,animation,pScope){
-  scale(0.1)
-  pScope.draw_image("heart1",x,y);
+  var layer1 = new PLayer(stars);
+  layer1.mode(SWIRL(1));
+  layer1.set_boundary( 0, 800 );
   
+  var layer2 = new PLayer(edge);
+  layer2.mode(RING);
+  layer2.set_boundary( 800, 1000 );
+
 }
 
-function kissy(x,y,animation,pScope){
-  scale(0.1)
+function stars(x, y, animation, pScope){
+  scale(0.2);
+  pScope.draw_image("streak",x,y);
   
-  pScope.draw_image("kissy",10,1000-animation.wave()*50,20,20);
-}
-
-function faces(x, y, animation, pScope){
   
-  scale(animation.frame*2);
-
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
 
 }
 
-function squares(x, y, animation, pScope){
-
-  // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
-
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
-
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
-
-}
+function edge(x, y, animation, pScope){
+push()
+  rotate(90 * animation.frame)
+  let rectJump = 750 + (animation.wave(1) * 50)
+  rect(150, rectJump, 100,100)
+pop()
+} 

fe61bc3503d80c082fa2144f35a7a9e995ad7d30
oliviawhiteman <oliviarosew04@gmail.com>
2023-09-11 23:54:26 +1200

    starting the final design idea

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,4 +1,4 @@
-const SLICE_COUNT = 15;
+const SLICE_COUNT = 12;
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
@@ -6,51 +6,35 @@ function setup_pScope(pScope){
   pScope.draw_layer_boundaries(true);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
-  pScope.load_image("streak" , "png");
-  pScope.load_image("moon" , "png");
+  pScope.load_image("lilypad" , "png");
+  pScope.load_image_sequence("fish" , "png",16 )
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null,11, 11, 38);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null,104, 204, 232);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(stars);
-  layer1.mode(SWIRL(10));
-  layer1.set_boundary( 0, 1000 );
   
-  var layer2 = new PLayer(edge);
-  layer2.mode(RING);
-  layer2.set_boundary( 800, 1000 );
-  
-  var layer3 = new PLayer(moon);
-  layer3.mode(RING);
-  layer3.set_boundary(0,300);
+  var layer1 = new PLayer(lilypad);
+  layer1.mode(RING);
+  layer1.set_boundary(0,300);
 
+  var fishsequence = new PLayer(fish);
+  fishsequence.mode(RING);
+  fishsequence.set_boundary(0,1000);
 
 }
 
-function stars(x, y, animation, pScope){
-  scale(0.2);
-  pScope.draw_image("streak",x,y);
-  
-  
-
-}
-
-function edge(x, y, animation, pScope){
-push()
-  rotate(90 * animation.frame)
-  let rectJump = 750 + (animation.wave(1) * 50)
-  rect(150, rectJump, 100,100)
-pop()
-}
-
-function moon (x,y,animation,pScope){
+function lilypad (x,y,animation,pScope){
   push()
-  scale(0.6)
   if(animation.frame == 0){
-  pScope.draw_image("moon",x,y);
+  pScope.draw_image("lilypad",x,y);
  
   }
    pop()
-} 
+}
+function fish(x,y,animation,pScope){
+  scale(0.5)
+  pScope.draw_image_from_sequence("fish", x, 1000,animation.frame);
+}
+
